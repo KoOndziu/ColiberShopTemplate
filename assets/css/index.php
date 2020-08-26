fbc1a4525608dccb39dca696cae6d5c83c49c64c
Ko_Ondziu <Ko_Ondziu@K-DELL-7590>
2020-08-26 21:03:00 +0200

    add index files in dev and assets

---
+++ assets/css/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 2019-2020 Konrad Kubala
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the AFL-3.0
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @author    Konrad Kubala <000konrad000@gmail.com>
+ * @copyright 2007-2020 Konrad Kubala <000konrad000@gmail.com>
+ * @license   AFL-3.0
+ */
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
