4aa33af8466b34684ee7a22939681d3a5738c23b
Ko_Ondziu <Ko_Ondziu@K-DELL-7590>
2020-08-26 03:45:34 +0200

    js license fix and delete some of files

--- _dev/js/components/ajax.js
+++ _dev/js/components/ajax.js
@@ -1,8 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/** 
+ * 2019-2020 Konrad Kubala <000konrad000@gmail.com>
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the AFL-3.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * 
+ * @author    Konrad Kubala <000konrad000@gmail.com>
+ * @copyright 2007-26 sie 2020 Konrad Kubala <000konrad000@gmail.com>
+ * @license   AFL-3.0
  */
+
 export default class Ajax {
   constructor(promises = []){ 
     if(Ajax.exists){ 
