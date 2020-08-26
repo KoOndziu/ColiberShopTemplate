d3b526032436ac52c55e2c978f8e4c7f3ada437a
Ko_Ondziu <Ko_Ondziu@K-DELL-7590>
2020-08-26 03:57:04 +0200

    js license fix 3

--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,3 +1,16 @@
+/** 
+ * 2019-2020 Konrad Kubala <000konrad000@gmail.com>
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the AFL-3.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * 
+ * @author    Konrad Kubala <000konrad000@gmail.com>
+ * @copyright 2007-2020 Konrad Kubala <000konrad000@gmail.com>
+ * @license   AFL-3.0
+ */
+
 var path = require('path');
 var webpack = require('webpack');
 var MiniCss = require("mini-css-extract-plugin");
