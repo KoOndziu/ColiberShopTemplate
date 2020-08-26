5e5ee8f030246a81d673878cb6b3ed4922ec3e82
Ko_Ondziu <Ko_Ondziu@K-DELL-7590>
2020-08-26 03:50:54 +0200

    js license fix and delete some of files 2

--- _dev/js/product.js
+++ _dev/js/product.js
@@ -7,7 +7,7 @@
  * that is bundled with this package in the file LICENSE.txt.
  * 
  * @author    Konrad Kubala <000konrad000@gmail.com>
- * @copyright 2007-26 sie 2020 Konrad Kubala <000konrad000@gmail.com>
+ * @copyright 2007-2020 Konrad Kubala <000konrad000@gmail.com>
  * @license   AFL-3.0
  */
 
